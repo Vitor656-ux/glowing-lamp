95c3fdd9545e7c89c85f40b4124adae094e1c379
Vitor656-ux <00001215173775sp@al.educacao.sp.gov.br>
2025-04-29 12:13:22 -0300

    Add files via upload

---
+++ function.py
@@ -0,0 +1,32 @@
+def soma():
+    nun_01 = 25
+    nun_02 = 31
+
+    resultado = nun_01 = + nun_02
+    
+    return resultado
+
+soma()
+
+print(soma())
+
+def somarDoisNumeros(nota_01, nota_02):
+    resultado = nota_01 + nota_02
+
+    nun_01 = nota_01
+    nun_02 = nota_02
+
+    resultado = nun_01 + nun_02
+
+    return print(resultado)
+
+# somarDoisNumeros(24, 28)
+
+def cadastrar (nome, idade):
+    data = {
+        "nome": nome,
+        "idade": idade
+    }
+
+    return print(data)
+cadastrar("Vitor"), 
